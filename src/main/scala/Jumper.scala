3a290c126f3c7c478d1508840ad039581cd417ef
Pankaj Gupta <pankajg@twitter.com>
2014-08-07 01:06:19 -0700

    Implement persistence of project search index on disk.

--- src/main/scala/Jumper.scala
+++ src/main/scala/Jumper.scala
@@ -6,9 +6,10 @@ import java.util.concurrent.ConcurrentLinkedQueue
 import com.pankaj.jump.{AltJumpService, JumpDecider, JumpHandler, FindHandler, Path}
 import com.pankaj.jump.parser.{Parser, ParserFactory, ParseWorker}
 import com.pankaj.jump.fs.{DirtFinder, DiskCrawler, RootsTracker}
-import com.pankaj.jump.db.{Db, FileTable, IdentTable, InvertedIdentIndex, RootsTable, SymbolTable}
+import com.pankaj.jump.db._
 import com.pankaj.jump.util.{ThreadNonBlockingActor, ThreadBlockingActor}
 import com.pankaj.jump.ident.{IdentRecorder, IdentSearcher}
+import java.io.{File, FileInputStream}
 
 object Jumper {
   def main(args:Array[String]): Unit = {
@@ -18,7 +19,19 @@ object Jumper {
     val rootsTable = new RootsTable(db)
     val symbolTable = new SymbolTable(db, fileTable)
     val identTable = new IdentTable(db)
-    val identIndex = new InvertedIdentIndex
+    val identIndex = {
+      val indexFileName = InvertedIdentIndexSerializer.indexFileName
+      val indexFile = new File(indexFileName)
+      if (indexFile.exists) {
+        println(s"reading bloom index $indexFileName")
+        val is = new FileInputStream(indexFile)
+        val index = InvertedIdentIndex.deserialize(is)
+        is.close()
+        index
+      } else {
+        new InvertedIdentIndex
+      }
+    }
     fileTable.setUp()
     rootsTable.setUp()
     symbolTable.setUp()
@@ -37,18 +50,22 @@ object Jumper {
       new DiskCrawler(rootsTracker, fileTable, dirtFinderActor)
     )
     diskCrawlerActor.start()
+    val indexWriterActor = new ThreadNonBlockingActor(new InvertedIdentIndexSerializer)
+    indexWriterActor.start()
+    indexWriterActor.send(identIndex)
 
     val timer = new ScheduledThreadPoolTimer()
     val crawl_period_env = System.getenv("DISK_CRAWL_PERIOD")
     val crawl_period =
       if (crawl_period_env != null) crawl_period_env.toInt
       else 60
-    timer.schedule(crawl_period.minutes) {
+    timer.schedule(crawl_period.seconds) {
       try {
         diskCrawlerActor.send(())
         //fileTable.printFiles()
         //rootsTable.printRoots()
         dirtFinderActor.send(())
+        indexWriterActor.send(identIndex)
         //symbolTable.printAll()
       } catch {
         case e: Throwable =>
--- src/main/scala/com/pankaj/jump/db/InvertedIdentIndexSerializer.scala
+++ src/main/scala/com/pankaj/jump/db/InvertedIdentIndexSerializer.scala
@@ -1,8 +1,28 @@
 package com.pankaj.jump.db
 
-class InvertedIdentIndexSerializer {
-  def serialize(): Unit = {
+import java.io.{File, FileOutputStream, FileInputStream}
 
+object InvertedIdentIndexSerializer {
+  val homeDir = System.getProperty("user.home")
+  val indexFileName = s"$homeDir/.jumpFindIndex"
+}
+
+
+class InvertedIdentIndexSerializer
+extends (InvertedIdentIndex => Unit) {
+  import InvertedIdentIndexSerializer._
+
+  def apply(index: InvertedIdentIndex): Unit = {
+    // write to a temporary file first
+    // them replace the existing file with it
+    val temp = File.createTempFile("temp",".index");
+    val fos = new FileOutputStream(temp)
+    InvertedIdentIndex.serialize(index, fos)
+    fos.close
+    val indexFile = new File(indexFileName)
+    indexFile.delete()
+    temp.renameTo(indexFile)
+    println(s"written bloom index $indexFileName")
   }
 }
 
